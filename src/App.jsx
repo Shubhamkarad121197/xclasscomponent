be738d2c34d0d8851704f1fb3c8a93c74d79c627
shubham karad <shubham.karad12@gmail.com>
2025-09-10 22:19:59 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+
+import './App.css'
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    };
+  }
+
+  increment = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  decrement = () => {
+    this.setState({ count: this.state.count - 1 });
+  };
+
+  reset = () => {
+    this.setState({ count: 0 });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Counter using Class Component</h1>
+        <h2>{this.state.count}</h2>
+        <button onClick={this.increment}>Increment</button>
+        <button onClick={this.decrement}>Decrement</button>
+        {/* <button onClick={this.reset}>Reset</button> */}
+      </div>
+    );
+  }
+}
+
+export default App;
+
